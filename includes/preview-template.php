a3ceee0a0d885ba74fa50ccdbb2ea075d8643cd7
Caleb Barnes <caleb@bare.ca>
2020-08-27 14:20:59 -0700

    wp constant GATSBY_PREVIEW_FRONTEND_URL

--- includes/preview-template.php
+++ includes/preview-template.php
@@ -1,4 +1,4 @@
-<?php wp_head(); 
+<?php wp_head();
 
 global $post;
 
@@ -6,13 +6,13 @@ console_log($post);
 
 $post_id = $post->ID;
 $post_type = $post->post_type;
-$revision = array_values( wp_get_post_revisions( $post_id ) )[0] ?? null;
+$revision = array_values(wp_get_post_revisions($post_id))[0] ?? null;
 console_log($revision);
 $revision_id;
 
 if (isset($revision)) {
     $revision_id = $revision->ID;
-} 
+}
 // else if (isset($post_id)) {
 //     $revision_id = $post_id;
 // }
@@ -25,7 +25,7 @@ console_log(['revision_id' => $revision_id]);
 
 if (function_exists('graphql')) {
     $graphql = graphql([
-                'query' => "{
+        'query' => "{
                                 viewer {
                                     jwtAuthToken
                                 }
@@ -33,7 +33,7 @@ if (function_exists('graphql')) {
                                     id
                                 }
                             }"
-                ]);
+    ]);
 
     console_log($graphql);
 
@@ -59,30 +59,30 @@ if (function_exists('graphql')) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Preview</title>
     <style>
-    /* #wpadminbar,
+        /* #wpadminbar,
     .gp-actions {
         display: none;
     } */
 
-    iframe {
-        position: fixed;
-        top: 32px;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
+        iframe {
+            position: fixed;
+            top: 32px;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
     </style>
 </head>
 
 <body>
-    <!-- <?php $frontend_url = GATSBY_PREVIEW_FRONTEND_URL; ?> -->
-    <?php $frontend_url = 'https://wp-gatsby-demo-1.netlify.app'; ?>
+    <!-- <?php $frontend_url = 'https://wp-gatsby-demo-1.netlify.app'; ?> -->
+    <?php $frontend_url = GATSBY_PREVIEW_FRONTEND_URL; ?>
     <?php $frontend_url_trailing_slash = rtrim($frontend_url, '/') . '/'; ?>
     <?php $frontend_url_no_trailing_slash = rtrim($frontend_url, '/'); ?>
 
-    <?php if ($frontend_url): ?>
-    <iframe id='preview' src="<?= $frontend_url_trailing_slash;
-     ?>preview/types/<?= $post_type; ?>?id=<?= $graphql_page_id ?>&key=<?= $jwtAuthToken; ?>&no-cache=1" frameborder="0"></iframe>
+    <?php if ($frontend_url) : ?>
+        <iframe id='preview' src="<?= $frontend_url_trailing_slash;
+                                    ?>preview/types/<?= $post_type; ?>?id=<?= $graphql_page_id ?>&key=<?= $jwtAuthToken; ?>&no-cache=1" frameborder="0"></iframe>
     <?php endif; ?>
 </body>
 
