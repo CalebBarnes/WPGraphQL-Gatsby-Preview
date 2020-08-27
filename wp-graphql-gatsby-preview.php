b64159427a63fd2c4c6c2a401d25b490cca65a41
Caleb Barnes <caleb@bare.ca>
2020-08-27 14:18:11 -0700

    Update wp-graphql-gatsby-preview.php

--- wp-graphql-gatsby-preview.php
+++ wp-graphql-gatsby-preview.php
@@ -10,33 +10,35 @@
  */
 
 
-require_once(plugin_dir_path( __FILE__ ) . 'includes/post-preview-button.php');
+// require_once(plugin_dir_path( __FILE__ ) . 'includes/post-preview-button.php');
+require_once(plugin_dir_path( __FILE__ ) . 'includes/console_log.php');
 
-/* Query Vars */
-add_filter( 'query_vars', 'add_preview_query_vars' );
-function add_preview_query_vars( $vars ) {
-    $vars[] = 'preview';
-    return $vars;
-}
+// /* Query Vars */
+// add_filter( 'query_vars', 'add_preview_query_vars' );
+// function add_preview_query_vars( $vars ) {
+//     $vars[] = 'preview';
+//     return $vars;
+// }
 
-/* Rewrite Rules */
-add_action('init', 'gatsby_preview_rewrite_rules');
-function gatsby_preview_rewrite_rules() {
-    add_rewrite_rule( 'preview/?$', 'index.php?preview=true', 'top' );
-}
+// /* Rewrite Rules */
+// add_action('init', 'gatsby_preview_rewrite_rules');
+// function gatsby_preview_rewrite_rules() {
+//     add_rewrite_rule( 'preview/?preview=true$', 'index.php?preview=true', 'top' );
+// }
 
 /* Template Include */
 add_filter('template_include', 'gatsby_preview_template_include', 1, 1); 
 function gatsby_preview_template_include($template)
 {
-    global $wp_query; //Load $wp_query object
-    $page_value = $wp_query->query_vars['preview']; //Check for query var "preview"
+    $is_preview  = is_preview();
+    console_log($is_preview);
 
-    if ($page_value && $page_value == "true") { //Verify "preview" exists and value is "true".
+    if ($is_preview) {
         return plugin_dir_path(__FILE__) . 'includes/preview-template.php'; //Load your template or file
     }
 
-    return $template; //Load normal template when $page_value != "true" as a fallback
+    return $template; 
 }
+
 
 ?>
